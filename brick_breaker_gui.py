eec703f314fd1f86e0539f74470ad7547ebea021
Merve Ozdas <mrvozdas@gmail.com>
2024-08-09 13:44:14 +0300

    Add assets and create board

---
+++ brick_breaker_gui.py
@@ -0,0 +1,43 @@
+import tkinter as tk
+import sys
+from os import path
+from PIL import Image, ImageTk
+
+
+class BrickBreaker(tk.Canvas):
+    def __init__(self):
+        super().__init__(width=600, height=600, background='black', highlightthickness=0)
+        self.paddle_position = [(250, 500)]
+
+        self.load_assets()
+        self.create_object()
+
+    def load_assets(self):
+        try:
+            bundle_dir = getattr(sys, "_MEIPASS", path.abspath(path.dirname(__file__)))
+            path_to_brick = path.join(bundle_dir, "assets", "brick.png")
+            path_to_paddle = path.join(bundle_dir, "assets", "paddle.png")
+            self.brick_image = Image.open(path_to_brick)
+            self.brick = ImageTk.PhotoImage(self.brick_image)
+
+            self.paddle_image = Image.open(path_to_paddle)
+            self.paddle = ImageTk.PhotoImage(self.paddle_image)
+
+        except IOError as e:
+            print(e)
+            root.destroy()
+
+    def create_object(self):
+        self.create_image(50, 50, image=self.brick, tag="brick")
+        self.create_image(self.paddle_position[0][0], self.paddle_position[0][1], image=self.paddle, tag="paddle")
+
+
+if __name__ == '__main__':
+    root = tk.Tk()
+    root.title('Brick Breaker')
+    root.resizable(False, False)
+
+    board = BrickBreaker()
+    board.pack()
+
+    root.mainloop()
