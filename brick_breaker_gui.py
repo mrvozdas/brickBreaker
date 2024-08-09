a0d4ee009dd0ea03ea29afc8c8c60cdc9e8f4026
Merve Ozdas <mrvozdas@gmail.com>
2024-08-09 14:02:02 +0300

    Add ball

--- brick_breaker_gui.py
+++ brick_breaker_gui.py
@@ -17,11 +17,16 @@ class BrickBreaker(tk.Canvas):
             bundle_dir = getattr(sys, "_MEIPASS", path.abspath(path.dirname(__file__)))
             path_to_brick = path.join(bundle_dir, "assets", "brick.png")
             path_to_paddle = path.join(bundle_dir, "assets", "paddle.png")
+            path_to_ball = path.join(bundle_dir, "assets", "ball.png")
+
             self.brick_image = Image.open(path_to_brick)
             self.brick = ImageTk.PhotoImage(self.brick_image)
 
             self.paddle_image = Image.open(path_to_paddle)
             self.paddle = ImageTk.PhotoImage(self.paddle_image)
+
+            self.ball_image = Image.open(path_to_ball)
+            self.ball = ImageTk.PhotoImage(self.ball_image)
 
         except IOError as e:
             print(e)
@@ -30,6 +35,7 @@ class BrickBreaker(tk.Canvas):
     def create_object(self):
         self.create_image(50, 50, image=self.brick, tag="brick")
         self.create_image(self.paddle_position[0][0], self.paddle_position[0][1], image=self.paddle, tag="paddle")
+        self.create_image(200, 200, image=self.ball, tag='ball')
 
 
 if __name__ == '__main__':
